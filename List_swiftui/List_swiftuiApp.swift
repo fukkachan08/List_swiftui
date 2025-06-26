8ea49acdb76cd7a9558176028230ce013cf38cf6
Fukano Masato <marbo0805@gmail.com>
2025-06-26 23:26:30 +0900

    Initial Commit

---
+++ List_swiftui/List_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_swiftuiApp.swift
+//  List_swiftui
+//
+//  Created by 深野真人 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct List_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
